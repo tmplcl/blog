a3d044514853d0d7b651e030892331c46ac2cc34
Timur Philip Cöl <timur.philip.coel@gmail.com>
2021-12-01 11:19:02 +0100

    cleanup and add readme

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# So this is the source for my blog
+
+- Written in markdown
+- Built with hugo
+- Deployed with CDK onto S3/ Cloudfront
--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -6,8 +6,8 @@ import { InfraStack as BlogInfraStack } from '../lib/infra-stack';
 const app = new cdk.App();
 new BlogInfraStack(app, 'BlogInfraStack', {
     env: {
-        account: '834067345082',
-        region: 'eu-central-1',
+        account: process.env.AWS_ACCOUNT_ID,
+        region: process.env.AWS_REGION,
     },
     domainName: "coel.link",
     siteSubDomain: "blog"
